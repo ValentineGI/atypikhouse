095c9e3d2ad7371e55a5e0c2e97b6f967908509d
Valentine GIRARDOT <valentine@spin-on.fr>
2021-12-02 17:00:53 +0100

    Test de plugin de réservation

--- wp-content/themes/atypikhouse/template-parts/header-links.php
+++ wp-content/themes/atypikhouse/template-parts/header-links.php
@@ -1,6 +1,6 @@
 <ul class="header-links">
     <li class="header-links_item">
-        <a href="<?php echo get_search_query(); ?>" target="<?php echo get_search_query(); ?>" title="<?php _e("Recherche", 'atypikhouse'); ?>">
+        <a href="<?php echo esc_url( get_page_link( 283 ) ); ?>" target="<?php echo get_search_query(); ?>" title="<?php _e("Recherche", 'atypikhouse'); ?>">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
         </a>
     </li>
@@ -10,8 +10,9 @@
         </a>
     </li>
     <li class="header-links_item">
-        <a href="#" target="#" title="<?php _e("User", 'atypikhouse'); ?>">
+        <a href="<?php echo esc_url( get_page_link( 283 ) ); ?>" target="#" title="<?php _e("User", 'atypikhouse'); ?>">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
         </a>
     </li>
 </ul>
+
--- wp-content/themes/atypikhouse/template-parts/home/home-recherche.php
+++ wp-content/themes/atypikhouse/template-parts/home/home-recherche.php
@@ -1,7 +1,7 @@
 <div class="reset-container relative z-20">
 	<div class="container">
 		<div class="widget-recherche -mt-20 p-5 shadow-2xl mx-6 bg-white">
-			<?php echo do_shortcode('[mphb_availability_search]'); ?>
+			<?php echo do_shortcode('[vikbooking category_id="" view="vikbooking" lang="fr-FR"]'); ?>
 		</div>
 	</div>
 </div>
